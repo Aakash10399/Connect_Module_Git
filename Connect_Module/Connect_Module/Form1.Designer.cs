3907e4952ae26ba56e824695966301058bd453a6
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-11-14 00:13:59 +0530

    UPDATE_1

---
+++ Connect_Module/Connect_Module/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Connect_Module
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(229, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 149);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(552, 113);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(235, 149);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(229, 338);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(225, 143);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(552, 337);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(235, 144);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(452, 527);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 530);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Whats the connect?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 524);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Next!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(452, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 717);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Connect_Module/Connect_Module/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Connect_Module
+{
+    public partial class Form1 : Form
+    {
+        public static string[] ans;
+        public static int c=0;
+        public static List<string> inputs;
+        public static int score=0;
+        public static int co = 0;
+        public static string shash, uname;
+        public static string click;
+        public Form1(string unamet, string shasht, int scoret, string clickt)
+        {
+            shash = shasht; uname = unamet; score = 0; click = clickt;
+            
+            InitializeComponent();
+            AutoScroll = true;
+            this.WindowState = FormWindowState.Maximized;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Public\\Main_App\\Connect_Module\\answers.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                c++;
+            }
+            file.Close();
+            ans = new string[c];
+            inputs = new List<string>(c);
+            c = 0;
+            System.IO.StreamReader file1 = new System.IO.StreamReader("C:\\Users\\Public\\Main_App\\Connect_Module\\answers.txt");          
+            while ((line = file1.ReadLine()) != null)
+            {
+                ans[c] = line;
+                c++;
+            }
+            file1.Close();
+            Game_Construct();
+        }
+        public void Game_Construct()
+        {
+            if(co<c)
+            { 
+            pictureBox1.ImageLocation = @"C:\\Users\\Public\\Main_App\\Connect_Module\\qa" + co.ToString();
+            pictureBox2.ImageLocation = @"C:\\Users\\Public\\Main_App\\Connect_Module\\qb" + co.ToString();
+            pictureBox3.ImageLocation = @"C:\\Users\\Public\\Main_App\\Connect_Module\\qc" + co.ToString();
+            pictureBox4.ImageLocation = @"C:\\Users\\Public\\Main_App\\Connect_Module\\qd" + co.ToString();
+            }
+            else
+            {
+                string[] temp = inputs.ToArray();
+                for(int i=0;i<ans.Length;i++)
+                {
+                    
+                    if(ans[i].Equals(inputs[i]))
+                    { score++; }
+                }
+                End_Screen();
+            }
+        }
+        public void End_Screen()
+        {
+            pictureBox1.Hide(); pictureBox2.Hide(); pictureBox3.Hide(); pictureBox4.Hide(); label1.Hide(); textBox1.Hide(); button1.Hide();
+            label2.Text = "FINAL SCORE : " + score.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            co++;
+            inputs.Add(textBox1.Text);
+            Game_Construct();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+
+                string url = "http://www.cofas10399.16mb.com/update_score.php?uname=" + uname + "&hash=" + shash + "&score=" + score.ToString() + "&gtype=" + click;
+                string response = client.DownloadString(url);
+                response = response.Trim();
+
+                if (response.Substring(0, 1).Equals("0"))
+                {
+                    Console.WriteLine("ERROR_SERVERSIDE");
+                }
+                if (response.Substring(0, 1).Equals("1"))
+                {
+                    Console.WriteLine("SUCCESS");
+                }
+
+
+            }
+            catch
+            {
+                Console.WriteLine("ERROR_INTERNET");
+            }
+        }
+    }
+}
